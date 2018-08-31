72edbceb78c8a95f597c0efb6b32119a8194c63f
Airic <epineda773@protonmail.com>
2018-08-31 00:28:51 -0700

    Update multiplicationTable.cs
    Same problem, different solution. Jagged Array vs Multi dimensional Array

--- multiplicationTable.cs
+++ multiplicationTable.cs
@@ -8,20 +8,40 @@ contain the following:
 			0 2 4 6 
 			0 3 6 9
 */
+ namespace Treehouse.CodeChallenges
+{
     public static class MathHelpers
     {
-        public static int[][] BuildMultiplicationTable(int maxFactor)
+        public static int[][] BuildMultiplicationTableJaggedArray(int maxFactor)
         {
-	     int [][] result = new int [maxFactor+1][];         
-           	 for(int i=0; i <= maxFactor; i++)
-		 {
-			 result[i] = new int[maxFactor+1];                    
-			 for(int j=0; j <= maxFactor; j++)
-			 {
-				 result[i][j] = i * j;
-                         }
-               	}
+			int [][] result = new int [maxFactor+1][];
+            
+            for(int i=0; i <= maxFactor; i++)
+            	{
+                	result[i] = new int[maxFactor+1];                    
+                        for(int j=0; j <= maxFactor; j++)
+                        {
+                            result[i][j] = i * j;
+                        }
+                }
                 return result;
+        }
+    }
+
+     public static class MathHelpers
+    {
+        public static int[,] BuildMultiplicationTable(int maxFactor)
+        {
+			int[,] result = new int [maxFactor+1 , maxFactor+1];
+            
+            for(int colIndex = 0; colIndex <= maxFactor; colIndex++)
+            {
+                for(int rowIndex = 0; rowIndex <= maxFactor; rowIndex++)
+                {
+                    result[rowIndex , colIndex] = rowIndex * colIndex;
+                }
+            }
+            return result;
         }
     }
 }
