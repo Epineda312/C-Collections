e0007d0d8f8727d58f9643df9497d8089a0e0004
Airic <epineda773@protonmail.com>
2018-08-31 00:04:04 -0700

    Update multiplicationTable.cs

--- multiplicationTable.cs
+++ multiplicationTable.cs
@@ -2,7 +2,11 @@
 Return a multiplication table consisting of a 2D multidimensional array. 
 The table should contain all of the products of integers from 0 to maxFactor. 
 For example, if maxFactor is 3 the resulting multiplication table should 
-contain the following: 0 0 0 0 0 1 2 3 0 2 4 6 0 3 6 9
+contain the following:
+			0 0 0 0 
+			0 1 2 3 
+			0 2 4 6 
+			0 3 6 9
 */
     public static class MathHelpers
     {
