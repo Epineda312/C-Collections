58be6f24e3ba48f35011feaab59d741092d1eeec
Airic <epineda773@protonmail.com>
2018-09-04 20:22:12 -0700

    Add files via upload

---
+++ Dictionary/Dictionary/MorseCodeTranslator/Program.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Treehouse
+{
+    class Program
+    {
+        public static void Main()
+        {
+            while(true)
+            {
+              Console.Write(": ");
+              string input = Console.ReadLine();
+              
+              if(string.IsNullOrWhiteSpace(input))
+              {
+                break;
+              }
+                 
+              string output = MorseCodeTranslator.ToMorse(input);            
+              Console.WriteLine(output);
+              }
+        }
+     } 
+ }
